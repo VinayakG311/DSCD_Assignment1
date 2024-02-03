4f39feec73c4b09c576dba7c7c757dd173a43cb9
Vinayak Goel <vinayak21113@iiitd.ac.in>
2024-02-03 13:00:32 +0530

    group register

--- Part2/User.py
+++ Part2/User.py
@@ -16,5 +16,8 @@ def connect_to_GroupServer():
     socket2 = context.socket(zmq.REQ)
     socket2.connect("tcp://localhost:8888")
 
-if __name__== "main":
+
+if __name__ == '__main__':
     connect_to_MessageServer()
+
+
