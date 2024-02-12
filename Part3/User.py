5b1f77f601bb3be08372d051c217db99e176549e
vinayak-77 <vinayak270603@gmail.com>
2024-02-12 14:06:21 +0530

    Persistent notifications done q3

--- Part3/User.py
+++ Part3/User.py
@@ -6,8 +6,13 @@ ipAddr = socket.gethostbyname(hostname)
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=ipAddr))
 channel = connection.channel()
 
+# result = channel.queue_declare(queue='',durable=True)
+# queue_name = result.method.queue
+
+queue_name = ""
+
 channel.queue_declare(queue='user_request')
-channel.queue_declare(queue='notification_queue',exclusive=True, durable=True)
+# channel.queue_declare(queue=queue_name)
 
 channel.exchange_declare(exchange="notifications",exchange_type='direct')
 
@@ -26,11 +31,11 @@ def updateSubscription(name,option,youtuber):
   
   if(option == 's'):
     print(youtuber)
-    channel.queue_bind(exchange="notifications",queue='notification_queue',routing_key=youtuber)
+    channel.queue_bind(exchange="notifications",queue=queue_name,routing_key=youtuber)
   
   else:
     print(youtuber)
-    channel.queue_unbind(exchange="notifications",queue='notification_queue',routing_key=youtuber)
+    channel.queue_unbind(exchange="notifications",queue=queue_name,routing_key=youtuber)
     
   print("Success, Request Sent")
     
@@ -39,13 +44,16 @@ def updateSubscription(name,option,youtuber):
 
 
 try:
-
+  
   if(len(sys.argv) == 2):
     name = sys.argv[1]
     request_obj = {"userName":name}
     request = json.dumps(request_obj)
     channel.basic_publish(exchange='', routing_key='user_request', body=request)
     print(f"Login request sent by {name}")
+    queue_name = f"{name}_notification"
+    channel.queue_declare(queue=queue_name,durable=True)
+    # queue_name = result.method.queue
 
     # connection.close()
     
@@ -55,10 +63,11 @@ try:
     name = sys.argv[1]
     option = sys.argv[2]
     youtuber = sys.argv[3]
-    
+    queue_name = f"{name}_notification"
+    channel.queue_declare(queue=queue_name,durable=True)
     updateSubscription(name,option,youtuber)
     
-  channel.basic_consume(queue='notification_queue', on_message_callback=notificationCallback, auto_ack=True)
+  channel.basic_consume(queue=queue_name, on_message_callback=notificationCallback, auto_ack=True)
   channel.start_consuming()
 
 except KeyboardInterrupt:
